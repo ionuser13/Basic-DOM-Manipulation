6d520501390a026acb677faed0de8f41cffd0a7b
userjohn <johnchacpi479@gmail.com>
2022-09-27 21:20:45 -0500

    first method to listen events from html

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 //create variables that represent the selectors of every HTML element
+//selectors
 const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2")
+const btn = document.querySelector("#btnCalcular");
+const result = document.querySelector("#result");
+
+function btnOnClick() {
+    const sum = parseInt(input1.value) + parseInt(input2.value)
+    result.innerText = "Resultado: " + sum
+
+}
